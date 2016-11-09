26b200af1dfc803683285dca5d29e1886ff4587b
Nero Sule <sulenero@gmail.com>
2016-11-09 18:28:52 +0000

    Moves all files to lists

--- generators/app/index.js
+++ generators/app/index.js
@@ -7,7 +7,7 @@ module.exports = yeoman.Base.extend({
   prompting: function () {
     // Have Yeoman greet the user.
     this.log(yosay(
-      'Welcome to the magnificent ' + chalk.red('generator-prospero') + ' generator!'
+      'Welcome to ' + chalk.red('generator-prospero') + ' - the Prospero Template Generator!'
     ));
     
     var prompts = [{
@@ -72,79 +72,64 @@ module.exports = yeoman.Base.extend({
     
   },
   createNewSolution:function(){
-    //var appname = this.props.appname;
+
     var templates = this.createTemplates();
     var appname = templates.appname;
-    this.log(appname);
-    this.log(templates);
-
-    // Root
-    this.scc('global.json', 'global.json', templates);
-    this.scc('NuGet.config', 'NuGet.config', templates);
-    this.scc('ConventionAspNetApplication.sln', appname + '.sln', templates);
-
-
-    // Core
-    this.scc('src/ConventionAspNetApplication.Core/ConventionAspNetApplication.Core.xproj', 'src/' + appname + '.Core/' + appname + '.Core.xproj', templates);
-    this.scc('src/ConventionAspNetApplication.Core/project.json', 'src/' + appname + '.Core/project.json', templates);
-    this.scc('src/ConventionAspNetApplication.Core/ConventionAspNetApplicationModelBuilderAlteration.cs', 'src/' + appname + '.Core/' + appname + 'ModelBuilderAlteration.cs', templates);
-    this.scc('src/ConventionAspNetApplication.Core/Properties/AssemblyInfo.cs', 'src/' + appname + '.Core/Properties/AssemblyInfo.cs', templates);
-
-    this.createWebProject(appname + '.Web');
-    this.createConsoleProject(appname + '.Service');
+    var globalFiles = [
+      'global.json',
+      'NuGet.config',
+      {'ConventionAspNetApplication.sln': appname + '.sln'}
+    ];
     
-    // // Web
-    // this.scc('src/ConventionAspNetApplication.Web/web.release.config', 'src/' + appname + '.Web/web.release.config', templates);
-    // this.scc('src/ConventionAspNetApplication.Web/Properties/launchSettings.json', 'src/' + appname + '.Web/Properties/launchSettings.json', templates);
-    // this.scc('src/ConventionAspNetApplication.Web/ConventionAspNetApplication.Web.xproj', 'src/' + appname + '.Web/' + appname + '.Web.xproj', templates);
-    // this.scc('src/ConventionAspNetApplication.Web/bundleconfig.json', 'src/' + appname + '.Web/bundleconfig.json', templates);
-    // this.scc('src/ConventionAspNetApplication.Web/Program.cs', 'src/' + appname + '.Web/Program.cs', templates);
-    // this.scc('src/ConventionAspNetApplication.Web/project.json', 'src/' + appname + '.Web/project.json', templates);
-    // this.scc('src/ConventionAspNetApplication.Web/Startup.cs', 'src/' + appname + '.Web/Startup.cs', templates);
-    // this.scc('src/ConventionAspNetApplication.Web/web.config', 'src/' + appname + '.Web/web.config', templates);
-    // this.scc('src/ConventionAspNetApplication.Web/appsettings.json', 'src/' + appname + '.Web/appsettings.json', templates);
+    var coreFiles = [
+      'ConventionAspNetApplication.Core.xproj',
+      'project.json',
+      {'ConventionAspNetApplicationModelBuilderAlteration.cs': appname + 'ModelBuilderAlteration.cs'},
+      'Properties/AssemblyInfo.cs'
+    ];
 
-    // this.scc('src/ConventionAspNetApplication.Web/wwwroot/css/site.css', 'src/' + appname + '.Web/wwwroot/css/site.css', templates);
-    // this.scc('src/ConventionAspNetApplication.Web/wwwroot/images/banner1.svg', 'src/' + appname + '.Web/wwwroot/css/banner1.svg', templates);
-    // this.scc('src/ConventionAspNetApplication.Web/wwwroot/images/banner2.svg', 'src/' + appname + '.Web/wwwroot/css/banner1.svg', templates);
-    // this.scc('src/ConventionAspNetApplication.Web/wwwroot/images/banner3.svg', 'src/' + appname + '.Web/wwwroot/css/banner1.svg', templates);
-    // this.scc('src/ConventionAspNetApplication.Web/wwwroot/images/banner4.svg', 'src/' + appname + '.Web/wwwroot/css/banner1.svg', templates);
+    var buildFiles = [
+      'build.cake',
+      'build.ps1',
+      'build.cmd'
+    ];
 
-    // this.scc('src/ConventionAspNetApplication.Web/wwwroot/js/site.js', 'src/' + appname + '.Web/wwwroot/js/site.js', templates);
-    // this.scc('src/ConventionAspNetApplication.Web/wwwroot/favicon.ico', 'src/' + appname + '.Web/wwwroot/favicon.ico', templates);
+    this.copyFilesToDestination(globalFiles, '', '');
+    this.copyFilesToDestination(buildFiles, '', '');
 
-    // this.scc('src/ConventionAspNetApplication.Web/Core/Factories/ConventionAspNetApplicationDbContextFactory.cs', 'src/' + appname + '.Web/Core/Factories/' + appname + 'DbContextFactory.cs', templates);
-    // this.scc('src/ConventionAspNetApplication.Web/Controllers/HomeController.cs', 'src/' + appname + '.Web/Controllers/HomeController.cs', templates);
-
-    // this.scc('src/ConventionAspNetApplication.Web/Views/Home/About.cshtml', 'src/' + appname + '.Web/Views/Home/About.cshtml', templates);
-    // this.scc('src/ConventionAspNetApplication.Web/Views/Home/Contact.cshtml', 'src/' + appname + '.Web/Views/Home/Contact.cshtml', templates);
-    // this.scc('src/ConventionAspNetApplication.Web/Views/Home/Index.cshtml', 'src/' + appname + '.Web/Views/Home/Index.cshtml', templates);
-
-    // this.scc('src/ConventionAspNetApplication.Web/Views/Shared/_Layout.cshtml', 'src/' + appname + '.Web/Views/Shared/_Layout.cshtml', templates);
-    // this.scc('src/ConventionAspNetApplication.Web/Views/Shared/Error.cshtml', 'src/' + appname + '.Web/Views/Shared/Error.cshtml', templates);
-
-    // this.scc('src/ConventionAspNetApplication.Web/Views/_ViewImports.cshtml', 'src/' + appname + '.Web/Views/_ViewImports.cshtml', templates);
-    // this.scc('src/ConventionAspNetApplication.Web/Views/_ViewStart.cshtml', 'src/' + appname + '.Web/Views/_ViewStart.cshtml', templates);
-
-    this.scc('test/ConventionAspNetApplication.Tests/ConventionAspNetApplication.Tests.xproj', 'test/' + appname + '.Tests/' + appname + '.Tests.xproj', templates);
-    this.scc('test/ConventionAspNetApplication.Tests/project.json', 'test/' + appname + '.Tests/project.json', templates);
-    this.scc('test/ConventionAspNetApplication.Tests/SanitySpec.cs', 'test/' + appname + '.Tests/SanitySpec.cs', templates);
-    this.scc('test/ConventionAspNetApplication.Tests/Properties/AssemblyInfo.cs', 'test/' + appname + '.Tests/Properties/AssemblyInfo.cs', templates);
-
-    this.scc('build.cake', 'build.cake', templates);
-    this.scc('build.ps1', 'build.ps1', templates);
-    this.scc('build.cmd', 'build.cmd', templates)
+    this.copyFilesToDestination(coreFiles, 'src/ConventionAspNetApplication.Core/', 'src/' + appname + '.Core/');
+    this.createWebProject(appname, 'Web');
+    this.createConsoleProject(appname, 'Service');
+    this.createTestProject(appname)
   },
 
-  createWebProject:function(appName){
+  createTestProject:function(solutionName, testName) {
     // There's an issue where for some reason an additional project with 'undefined' is created...
-    if(appName === undefined) return;
-    var destinationPath = 'src/' + appName + '/';
+    if(solutionName === undefined) return;
+    var destinationFolderExtension = testName === undefined ? '' : '.' + testName;  
+    var destinationPath = 'test/' + solutionName + destinationFolderExtension + '.Tests' + '/';
+    var sourcePath = 'test/ConventionAspNetApplication.Tests/';
+
+    var files = [
+      {
+        'ConventionAspNetApplication.Tests.xproj':solutionName + (testName || '') + '.Tests.xproj'
+      },
+      'project.json',
+      'SanitySpec.cs',
+      'Properties/AssemblyInfo.cs'
+    ];
+    this.copyFilesToDestination(files, sourcePath, destinationPath);
+  },
+
+  createWebProject:function(solutionName, appName){
+    // There's an issue where for some reason an additional project with 'undefined' is created...
+    if(solutionName === undefined) return;
+    var destinationPath = 'src/' + solutionName + '.' + appName + '/';
     var sourcePath = 'src/ConventionAspNetApplication.Web/';
     var files = [
      {
-        'ConventionAspNetApplication.Web.xproj':appName + '.Web.xproj',
-        'Core/Factories/ConventionAspNetApplicationDbContextFactory.cs':'Core/Factories/' + appName + 'DbContextFactory.cs'
+        'ConventionAspNetApplication.Web.xproj':solutionName + '.' + appName + '.xproj',
+        'Core/Factories/ConventionAspNetApplicationDbContextFactory.cs':'Core/Factories/' + solutionName + appName + 'DbContextFactory.cs'
      },
 
      'Program.cs',
@@ -180,10 +165,33 @@ module.exports = yeoman.Base.extend({
      'Views/_ViewStart.cshtml'
     ];
 
+    this.copyFilesToDestination(files, sourcePath, destinationPath);
+  },
+
+  createConsoleProject:function(solutionName, appName){
+    // There's an issue where for some reason an additional project with 'undefined' is created...
+    if(solutionName === undefined) return;
+    var destinationPath = 'src/' + solutionName + '.' + appName + '/';
+    var sourcePath = 'src/ConventionAspNetApplication.Service/';
+
+    var files = [
+      {
+        'ConventionAspNetApplication.Service.xproj':solutionName + '.' + appName + '.xproj',
+        'ConventionAspNetApplicationService.cs':solutionName + appName + '.cs'
+      },
+      'project.json',
+      'appsettings.json',
+      'run.cmd',
+      'Program.cs',
+      'Properties/AssemblyInfo.cs'
+    ];
+    this.copyFilesToDestination(files, sourcePath, destinationPath);
+  },
+
+  copyFilesToDestination:function(files, sourcePath, destinationPath){
     for(var index in files)
     {
       var element = files[index];
-      this.log(element);
       if(typeof(element) === 'string'){
         this.scc(sourcePath + element, destinationPath + element);
       } else {
@@ -191,29 +199,6 @@ module.exports = yeoman.Base.extend({
           this.scc(sourcePath + key, destinationPath + element[key]);
       }
     }
-  },
-
-
-  createConsoleProject:function(appName){
-    // There's an issue where for some reason an additional project with 'undefined' is created...
-    if(appName === undefined) return;
-    var destinationPath = 'src/' + appName + '/';
-    var sourcePath = 'src/ConventionAspNetApplication.Service/';
-    this.log(appName);
-    this.log(destinationPath);
-    var scc = this.scc;
-    var copyFile = function(sourceFile, destinationFile) {
-      destinationFile = destinationFile || sourceFile;
-      this.scc(sourcePath + sourceFile, destinationPath + destinationFile);
-    }.bind(this);
-
-    copyFile('ConventionAspNetApplication.Service.xproj', appName + '.xproj');
-    copyFile('project.json');
-    copyFile('appsettings.json')
-    copyFile('run.cmd');
-    copyFile('Program.cs');
-    copyFile('ConventionAspNetApplicationService.cs', appName + 'Service.cs');
-    copyFile('Properties/AssemblyInfo.cs');
   },
 
   scc: function(tp, dp) {
